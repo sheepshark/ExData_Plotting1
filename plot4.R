ae9df4cf22d7eeddb7bc5ab2de5a18fbf944ed6a
Stewart Smith <sheepshark@users.noreply.github.com>
2017-05-07 21:34:59 -0500

    Add files via upload
    added code for reading data

--- plot4.R
+++ plot4.R
@@ -1,3 +1,31 @@
+if (!file.exists("power_consumption")) {
+        dir.create("power_consumption")
+}
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+file <- file.path("power_consumption.zip")
+
+if (!file.exists(file)) {
+        download.file(fileUrl, file)
+        dateDownloaded <- Sys.time()
+        unzip(file, exdir = "power_consumption")
+}
+
+power_consumption <- read.delim(
+        "./power_consumption/household_power_consumption.txt", header = TRUE, 
+        sep = ";", na.strings = "?")
+
+power_consumption_sub <- power_consumption[(power_consumption$Date == 
+                                                    "1/2/2007" | power_consumption$Date == "2/2/2007"),]
+
+#format date and time
+power_consumption_sub$Date <- as.Date(power_consumption_sub$Date, format="%d/%m/%Y")
+
+datetime <- paste(power_consumption_sub$Date, power_consumption_sub$Time)
+
+power_consumption_sub$DateTime <- as.POSIXct(datetime)
+
 ## Plot 4
 par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
 
